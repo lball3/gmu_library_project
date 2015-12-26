ccd9f76caa4d2ac18a15734460a54042a2dc7e38
Louis Ball <louis.ball@me.com>
2015-12-26 15:13:58 -0800

    final

--- library/app/controllers/users_controller.rb
+++ library/app/controllers/users_controller.rb
@@ -18,7 +18,6 @@ class UsersController < ApplicationController
   # GET /users/1/edit
   def edit
   end
-
   
   def create
     @user = User.new(user_params)
@@ -26,29 +25,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
-    end
-  end
-
-  # PATCH/PUT /users/1
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
+      redirect_to :action => '/signup'
     end
   end
 
