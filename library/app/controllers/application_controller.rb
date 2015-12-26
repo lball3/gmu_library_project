2b42d563551642c8cec99f339d81100ce4630148
Louis Ball <louis.ball@me.com>
2015-12-26 13:10:26 -0800

    sign in page done

--- library/app/controllers/application_controller.rb
+++ library/app/controllers/application_controller.rb
@@ -2,17 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
-  
-  helper_method :current_user
-  
-  def current_user
-    User.find(session[:user_id]) 
-  end
-  
 
-  def authorize
-    redirect_to '/login' unless current_user
-  end
-  
+  before_action :authorize
+
+  helper_method :current_user
+ 
+  protected
+    def authorize
+  	  unless User.find_by(id: session[:user_id])
+  		redirect_to new_session_path, notice: "Please log in"
+      end
+    end 
 end
--- library/app/controllers/sessions_controller.rb
+++ library/app/controllers/sessions_controller.rb
@@ -1,16 +1,24 @@
- def create
-    user = User.find_by_email(params[:session][:user_id])
-    if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      flash[:success] = "You have successfully logged in."
-      redirect_to '/'
-    else
-      flash[:error] = "Invalid username/password."
-      redirect_to new_session_path
-    end
+ class SessionsController < ApplicationController
+  skip_before_action :authorize
+  def new
   end
+
+  def create
+  	user = User.find_by(user_id: params[:session][:user_id])
+  	if user and user.authenticate(params[:session][:password])
+  		session[:user_id] = user.id
+  		flash[:success] = "You have successfully logged in."
+  		redirect_to books_url
+  	else
+  		flash[:error] = "Invalid username/password."
+  		redirect_to new_session_path
+  	end
+  end
+
 
   def destroy
     session[:user_id] = nil
     redirect_to new_session_path, notice: "Logged out"
   end
+
+end
